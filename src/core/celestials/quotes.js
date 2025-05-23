60b19f9f87b18c092ec3c957d5e2e794ddee96cd
Supersonic Seven <b.rockholz@icloud.com>
2025-05-23 04:38:54 -0400

    Update quotes.js
    First edit

--- src/core/celestials/quotes.js
+++ src/core/celestials/quotes.js
@@ -175,4 +175,8 @@ export const Quotes = {
     GameDatabase.celestials.quotes.pelle,
     config => new CelQuotes(config, "pelle")
   ),
+  alpha: mapGameDataToObject(
+    GameDatabase.celestials.quotes.alpha,
+    config => new CelQuotes(config, "alpha")
+  ),
 };
