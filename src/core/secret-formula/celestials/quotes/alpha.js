529d1a115ea3921486b91db01fc12a79099a06ef
Supersonic Seven <b.rockholz@icloud.com>
2025-05-23 05:02:08 -0400

    Create alpha.js
    quotes?

---
+++ src/core/secret-formula/celestials/quotes/alpha.js
@@ -0,0 +1,18 @@
+export const alphaQuotes = {
+  beginning: {
+    id: 0,
+    requirement: () => player.records.totalAntimatter.exponent = 1,
+    lines: [
+      "...",
+      "Hi.",
+      "You may not know me.",
+      "Or you may.",
+      "My name is Alpha, Celestial of Darkness.",
+      "And I have a challenge for you.",
+      "What is the challenge you ask? Why give that away?",
+      "Trust me, it would be much better if you saw for yourself.",
+      "Anyway, let's see how you do.",
+      "I'll be waiting.",
+    ]
+  },
+};
