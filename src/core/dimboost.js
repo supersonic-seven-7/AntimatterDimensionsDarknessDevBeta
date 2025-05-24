a3c672dad05af3a9f18de34b09ecdda92a0dbed5
Supersonic Seven <b.rockholz@icloud.com>
2025-05-24 15:30:09 -0400

    Update dimboost.js
    Small nerf but the pain has arrived

--- src/core/dimboost.js
+++ src/core/dimboost.js
@@ -19,7 +19,7 @@ export class DimBoost {
     }
 
     let boost = Effects.max(
-      4,
+      3,
       InfinityUpgrade.dimboostMult,
       InfinityChallenge(7).reward,
       InfinityChallenge(7),
