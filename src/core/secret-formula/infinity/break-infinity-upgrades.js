48a2f16da2fc8c5b8723fe025d9d2bbf9a7f7087
Supersonic Seven <b.rockholz@icloud.com>
2025-05-25 23:32:15 -0400

    Update break-infinity-upgrades.js
    lol cap error

--- src/core/secret-formula/infinity/break-infinity-upgrades.js
+++ src/core/secret-formula/infinity/break-infinity-upgrades.js
@@ -65,7 +65,7 @@ export const breakInfinityUpgrades = {
     id: "challengeMult",
     cost: 1e7,
     description: "Antimatter Dimensions gain a multiplier based on how fast your slowest challenge run is",
-    effect: () => Decimal.clampMin(Math.pow((400 / Time.worstChallenge.totalMinutes), 2), 1),
+    effect: () => Decimal.clampMin(Math.pow((50 / Time.worstChallenge.totalMinutes), 2), 1),
     formatEffect: value => formatX(value, 2, 2),
     hasCap: true,
     cap: DC.D2E9
