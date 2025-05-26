3698f9fe448e565c9906b5384c7e31284a63b9ee
Supersonic Seven <b.rockholz@icloud.com>
2025-05-26 17:56:18 -0400

    Update infinity-challenges.js
    IC2 possible

--- src/core/secret-formula/challenges/infinity-challenges.js
+++ src/core/secret-formula/challenges/infinity-challenges.js
@@ -17,7 +17,7 @@ export const infinityChallenges = [
   {
     id: 2,
     description: `all Normal Challenge restrictions are active at once except only the Big Crunch Autobuyer (C12) Challenge.`,
-    goal: DC.E9000,
+    goal: DC.E1350,
     isQuickResettable: true,
     reward: {
       description: () => `Improve the Infinity Power Conversion Rate based on Dimboosts`,
