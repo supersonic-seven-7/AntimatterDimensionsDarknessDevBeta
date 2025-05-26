f46e694476b210d6e027582ba2c692903c177ed7
Supersonic Seven <b.rockholz@icloud.com>
2025-05-26 16:25:40 -0400

    Update infinity-dimension.js
    Ok

--- src/core/dimensions/infinity-dimension.js
+++ src/core/dimensions/infinity-dimension.js
@@ -410,6 +410,6 @@ export const InfinityDimensions = {
 
   get powerConversionRate() {
     const multiplier = PelleRifts.paradox.milestones[2].effectOrDefault(1);
-    return (7 + getAdjustedGlyphEffect("infinityrate") + PelleUpgrade.infConversion.effectOrDefault(0)) * multiplier;
+    return (7 + InfinityChallenge(2).reward + getAdjustedGlyphEffect("infinityrate") + PelleUpgrade.infConversion.effectOrDefault(0)) * multiplier;
   }
 };
