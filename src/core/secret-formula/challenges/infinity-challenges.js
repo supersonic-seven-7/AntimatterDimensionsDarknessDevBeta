afced04005c0edb586b19f995c07f86d18809bde
Supersonic Seven <b.rockholz@icloud.com>
2025-05-26 18:21:04 -0400

    Update infinity-challenges.js
    Preparing IC4 (old IC3)

--- src/core/secret-formula/challenges/infinity-challenges.js
+++ src/core/secret-formula/challenges/infinity-challenges.js
@@ -44,7 +44,7 @@ export const infinityChallenges = [
     description: () =>
       `Tickspeed upgrades are always ${formatX(1)}. For every Tickspeed upgrade purchase, you instead get a static
       multiplier on all Antimatter Dimensions which increases based on Antimatter Galaxies.`,
-    goal: DC.E5000,
+    goal: DC.E9000,
     isQuickResettable: false,
     effect: () => Decimal.pow(1.05 + (player.galaxies * 0.005), player.totalTickBought),
     formatEffect: value => formatX(value, 2, 2),
@@ -55,7 +55,7 @@ export const infinityChallenges = [
         : Decimal.pow(1.05 + (player.galaxies * 0.005), player.totalTickBought)),
       formatEffect: value => formatX(value, 2, 2),
     },
-    unlockAM: DC.E12000,
+    unlockAM: DC.E10000,
   },
   {
     id: 5,
