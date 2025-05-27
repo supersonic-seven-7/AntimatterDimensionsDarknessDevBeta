aa21ad5dd133e9ac7b2b29b5a728a521562003ae
Supersonic Seven <b.rockholz@icloud.com>
2025-05-26 23:52:58 -0400

    Update infinity-challenges.js
    IC7 reward redone

--- src/core/secret-formula/challenges/infinity-challenges.js
+++ src/core/secret-formula/challenges/infinity-challenges.js
@@ -97,8 +97,8 @@ export const infinityChallenges = [
     effect: () => Currency.matter.value.clampMin(1),
     formatEffect: value => `/${format(value, 1, 2)}`,
     reward: {
-      description: "Infinity Dimension multiplier based on tickspeed",
-      effect: () => Tickspeed.perSecond.pow(0.0005),
+      description: "Dimboost multiplier based on tickspeed",
+      effect: () => Tickspeed.perSecond.pow(0.01),
       formatEffect: value => formatX(value, 2, 2)
     },
     unlockAM: DC.E24500,
