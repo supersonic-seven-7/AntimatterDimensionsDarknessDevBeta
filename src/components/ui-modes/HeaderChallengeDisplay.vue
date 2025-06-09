ffb1a9fd2ade31fa2dd1caeff064cfb6428f5c2d
Supersonic Seven <b.rockholz@icloud.com>
2025-06-09 00:20:01 -0400

    Update HeaderChallengeDisplay.vue
    Fuckall

--- src/components/ui-modes/HeaderChallengeDisplay.vue
+++ src/components/ui-modes/HeaderChallengeDisplay.vue
@@ -50,12 +50,12 @@ export default {
         {
           name: token => `Infinity Challenge ${token}`,
           isActive: token => token > 0,
-          activityToken: () => player.challenge.infinity.current !== 13
+          activityToken: () => player.challenge.infinity.current
         },
         {
           name: token => `Infinity Challenge ${token} (THE ULTIMATE CHALLENGE II)`,
           isActive: token => token > 0,
-          activityToken: () => player.challenge.infinity.current === 13
+          activityToken: () => player.challenge.infinity.current = 13
         },
         {
           name: token => `${NormalChallenge(token).config.name} Challenge`,
