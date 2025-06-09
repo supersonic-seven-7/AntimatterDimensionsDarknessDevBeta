0b979c4e48f3714330a711ead78682fb805a16ee
Supersonic Seven <b.rockholz@icloud.com>
2025-06-09 15:18:07 -0400

    Update normal-time-studies.js
    pls work

--- src/core/secret-formula/eternity/time-studies/normal-time-studies.js
+++ src/core/secret-formula/eternity/time-studies/normal-time-studies.js
@@ -111,8 +111,8 @@ export const normalTimeStudies = [
     cost: 18,
     requirement: [32],
     reqType: TS_REQUIREMENT_TYPE.AT_LEAST_ONE,
-    description: () => `Increase the Infinity Power Conversion Rate based on Eternities`,
-    effect: Math.pow(Currency.eternities.value.clampMax(1e100), 0.01),
+    description: () => `Increase the Infinity Power Conversion Rate based on Replicanti Boosters`,
+    effect: Math.pow(Replicanti.galaxies.total, 0.03),
     formatEffect: value => `+${format(value, 0, 2)}`
   },
   {
