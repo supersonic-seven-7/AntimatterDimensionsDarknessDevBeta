e687e2061bf31d457bdbfdc942c2d7f65fd4e392
Supersonic Seven <b.rockholz@icloud.com>
2025-06-09 23:35:20 -0400

    Update infinity-challenges.js
    womp

--- src/core/infinity-challenges.js
+++ src/core/infinity-challenges.js
@@ -1,7 +1,7 @@
 import { GameMechanicState } from "./game-mechanics";
 
 export function tryCompleteInfinityChallenges() {
-  if (EternityMilestone.autoIC.isReached) {
+  if (EternityMilestone.autoIC.isReached && InfinityChallenges.nextIC !== 13) {
     const toComplete = InfinityChallenges.all.filter(x => x.isUnlocked && !x.isCompleted);
     for (const challenge of toComplete) challenge.complete();
   }
