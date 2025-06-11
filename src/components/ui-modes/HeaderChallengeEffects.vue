94f807ca5a44a3a8d93505fda83ab73609ca8a3a
Supersonic Seven <b.rockholz@icloud.com>
2025-06-10 23:47:26 -0400

    Update HeaderChallengeEffects.vue
    Lol

--- src/components/ui-modes/HeaderChallengeEffects.vue
+++ src/components/ui-modes/HeaderChallengeEffects.vue
@@ -57,15 +57,16 @@ export default {
       const isC3Running = NormalChallenge(3).isRunning;
       const isIC6Running = InfinityChallenge(7).isRunning;
       const isIC8Running = InfinityChallenge(9).isRunning;
-      const isChallengePowerVisible = isC2Running || isC3Running || isIC6Running || isIC8Running;
+      const isIC13Running = InfinityChallenge(13).isRunning;
+      const isChallengePowerVisible = isC2Running || isC3Running || isIC6Running || isIC8Running  || isIC13Running;
       this.isChallengePowerVisible = isChallengePowerVisible;
       if (isChallengePowerVisible) {
         const powerArray = [];
         if (isC2Running) powerArray.push(`Production: ${formatPercents(player.chall2Pow, 2, 2)}`);
         if (isC3Running) powerArray.push(`First dimension: ${formatX(player.chall3Pow, 3, 4)}`);
-        if (isIC6Running) powerArray.push(`Matter: Antimatter Dimensions /
+        if (isIC6Running || isIC13Running) powerArray.push(`Matter: Antimatter Dimensions /
           ${format(new Decimal(1).timesEffectOf(InfinityChallenge(7)), 2, 2)}`);
-        if (isIC8Running) powerArray.push(`Production: /
+        if (isIC8Running || isIC13Running) powerArray.push(`Production: /
           ${format(new Decimal(1).timesEffectOf(InfinityChallenge(9)).reciprocal(), 2, 2)}`);
         this.challengePower = powerArray.join(", ");
       }
