10f43bbb7e1dafdf5ef900208f828e185b1fb4bd
Supersonic Seven <b.rockholz@icloud.com>
2025-06-13 17:20:03 -0400

    Update index.js
    This is why

--- src/core/secret-formula/challenges/index.js
+++ src/core/secret-formula/challenges/index.js
@@ -1,9 +1,11 @@
 import { eternityChallenges } from "./eternity-challenges";
 import { infinityChallenges } from "./infinity-challenges";
 import { normalChallenges } from "./normal-challenges";
+import { ultimateChallenges } from "./ultimate-challenges";
 
 export const challenges = {
   normal: normalChallenges,
   infinity: infinityChallenges,
-  eternity: eternityChallenges
+  eternity: eternityChallenges,
+  ultimate: ultimateChallenges
 };
