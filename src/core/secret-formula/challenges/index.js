a34bd16df736e3a3e66aac3d027a9f613e3d491f
Supersonic Seven <b.rockholz@icloud.com>
2025-06-13 17:27:00 -0400

    Update index.js
    L

--- src/core/secret-formula/challenges/index.js
+++ src/core/secret-formula/challenges/index.js
@@ -1,11 +1,9 @@
 import { eternityChallenges } from "./eternity-challenges";
 import { infinityChallenges } from "./infinity-challenges";
 import { normalChallenges } from "./normal-challenges";
-import { ultimateChallenges } from "./ultimate-challenges";
 
 export const challenges = {
   normal: normalChallenges,
   infinity: infinityChallenges,
-  eternity: eternityChallenges,
-  ultimate: ultimateChallenges
+  eternity: eternityChallenges
 };
