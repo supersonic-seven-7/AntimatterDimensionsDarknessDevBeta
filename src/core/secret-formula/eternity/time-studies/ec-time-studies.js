5dcfcf5a58191a3b6552c21ebeb3d088ede91d6a
Supersonic Seven <b.rockholz@icloud.com>
2025-06-19 15:44:24 -0400

    Update ec-time-studies.js
    yay

--- src/core/secret-formula/eternity/time-studies/ec-time-studies.js
+++ src/core/secret-formula/eternity/time-studies/ec-time-studies.js
@@ -21,7 +21,7 @@ export const ecTimeStudies = [
     secondary: {
       resource: "Tickspeed upgrades from Time Dimensions",
       current: () => player.totalTickGained,
-      required: completions => 8000 + Math.min(completions, 4) * 1000,
+      required: completions => 14300 + Math.min(completions, 4) * 2675,
       formatValue: formatInt
     }
   },
