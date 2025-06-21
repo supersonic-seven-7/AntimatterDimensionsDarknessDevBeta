dd8a9663e017f8b2d15d4861e2d90b9d3fa79ac0
Supersonic Seven <b.rockholz@icloud.com>
2025-06-21 12:59:39 -0400

    Update effarig.js
    Text8

--- src/core/secret-formula/celestials/effarig.js
+++ src/core/secret-formula/celestials/effarig.js
@@ -42,7 +42,7 @@ export const effarigUnlocks = {
     label: "Infinity",
     get description() {
       return ` Replicanti cap is multiplied by a value based on Infinities
-        Infinities increase your max Replicanti Galaxies
+        Infinities increase your max Replicanti Boosters
         Base Infinity Point gain is capped at ${format(DC.E200)} in Effarig's Reality
         Each type of Infinity Point multiplier is capped at ${format(DC.E50)} in Effarig's Reality`;
     },
