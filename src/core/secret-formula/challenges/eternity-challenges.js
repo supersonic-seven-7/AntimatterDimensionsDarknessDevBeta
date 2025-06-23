20a5d8856e487ed7aa140bcfa113ef7d55a42c10
Supersonic Seven <b.rockholz@icloud.com>
2025-06-23 08:36:50 -0400

    Update eternity-challenges.js
    Yay

--- src/core/secret-formula/challenges/eternity-challenges.js
+++ src/core/secret-formula/challenges/eternity-challenges.js
@@ -68,7 +68,7 @@ export const eternityChallenges = [
     description: () => `Dimension Boost costs scaling is massively increased.`,
     goal: DC.E10500,
     pelleGoal: DC.E1400,
-    goalIncrease: DC.E1E15,
+    goalIncrease: DC.E1600,
     reward: {
       description: "Improve the Eternity Point Formula",
       effect: completions => 3083 - (completions * 100),
