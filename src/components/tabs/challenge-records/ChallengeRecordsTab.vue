ab6ae8a5030579bc79398335a8b3f1be0d510347
Supersonic Seven <b.rockholz@icloud.com>
2025-08-19 17:23:53 -0400

    Update ChallengeRecordsTab.vue
    Record List

--- src/components/tabs/challenge-records/ChallengeRecordsTab.vue
+++ src/components/tabs/challenge-records/ChallengeRecordsTab.vue
@@ -9,16 +9,21 @@ export default {
   data() {
     return {
       infinityChallengesUnlocked: false,
+      ultimateChallengesUnlocked: false,
       normalChallenges: [],
-      infinityChallenges: []
+      infinityChallenges: [],
+      ultimateChallenges: []
     };
   },
   methods: {
     update() {
       this.infinityChallengesUnlocked = PlayerProgress.infinityChallengeCompleted() ||
         PlayerProgress.eternityUnlocked();
+      this.ultimateChallengesUnlocked = PlayerProgress.ultimateChallengeCompleted() ||
+        PlayerProgress.realityUnlocked();
       this.normalChallenges = player.challenge.normal.bestTimes.slice(0);
       this.infinityChallenges = player.challenge.infinity.bestTimes.slice(0);
+      this.ultimateChallenges = player.challenge.ultimate.bestTimes.slice(0);
     }
   }
 };
@@ -37,6 +42,13 @@ export default {
       :times="infinityChallenges"
       name="Infinity Challenge"
       class="l-challenge-records-tab__infinity_challenges"
+    />
+    <ChallengeRecordsList
+      v-if="ultimateChallengesUnlocked"
+      :start="1"
+      :times="ultimateChallenges"
+      name="Ultimate Challenge"
+      class="l-challenge-records-tab__ultimate_challenges"
     />
   </div>
 </template>
