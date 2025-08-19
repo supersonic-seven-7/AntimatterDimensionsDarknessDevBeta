d9dfc370146d1cc09cdc26b4370b0e984914db58
Supersonic Seven <b.rockholz@icloud.com>
2025-08-19 17:10:10 -0400

    Update ultimate-challenges.js
    Yes. Now it's broken.

--- src/core/secret-formula/challenges/ultimate-challenges.js
+++ src/core/secret-formula/challenges/ultimate-challenges.js
@@ -1,1 +1,27 @@
+import { DC } from "../../constants";
 
+export const infinityChallenges = [
+  {
+    id: 1,
+    description: () => `You are locked inside all Normal Challenges AT THE SAME TIME. This includes
+    Challenge ${formatInt(9)} and Challenge ${formatInt(12)}.`,
+    goal: Decimal.NUMBER_MAX_VALUE,
+    isQuickResettable: true,
+    reward: "You can Break Infinity",
+    unlockReq: PlayerProgress.eternityUnlocked(),
+  },
+  {
+    id: 2,
+    description: () =>
+      `You are trapped in every Infinity Challenge AT THE SAME TIME.`,
+    goal: DC.E1_6E6,
+    isQuickResettable: true,
+    reward: {
+      description:
+        "Delay Infinity Dimension Purchase Caps based on Dimension Boosts",
+      effect: () => Math.log10(DimBoost.purchasedBoosts + 1) + 1,
+      formatEffect: value => formatX(value, 2, 2)
+    },
+    unlockReq: DC.E3_6E6,
+  },
+];
