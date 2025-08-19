08cea51e5bf9e5a4b83d6dfa9b5051529e794eef
Supersonic Seven <b.rockholz@icloud.com>
2025-08-19 17:05:50 -0400

    Create UltimateChallengeTab.vue
    Concern

---
+++ src/components/tabs/ultimate-challenges/UltimateChallengeTab.vue
@@ -0,0 +1,61 @@
+<script>
+import ChallengeGrid from "@/components/ChallengeGrid";
+import ChallengeTabHeader from "@/components/ChallengeTabHeader";
+import UltimateChallengeBox from "./UltimateChallengeBox";
+
+export default {
+  name: "UltimateChallengesTab",
+  components: {
+    ChallengeGrid,
+    ChallengeTabHeader,
+    UltimateChallengeBox
+  },
+  data() {
+    return {
+      nextUC: 0,
+      showAllChallenges: false
+    };
+  },
+  computed: {
+    challenges() {
+      return UltimateChallenges.all;
+    },
+    nextAtDisplay() {
+      const first = this.nextUC?.id === 1;
+      const last = this.nextUC?.id === 2;
+      const next = UltimateChallenges.nextUCUnlockReq;
+
+      if (first) return `The first Ultimate Challenge unlocks at Eternity.`;
+      if (last) return "All Ultimate Challenges unlocked... so far";
+      return `Next Ultimate Challenge unlocks at ${format(next)} antimatter.`;
+    }
+  },
+  methods: {
+    update() {
+      this.nextUC = InfinityChallenges.nextUC;
+      this.showAllChallenges = player.options.showAllChallenges;
+    },
+    isChallengeVisible(challenge) {
+      return challenge.isUnlocked || (this.showAllChallenges && PlayerProgress.realityUnlocked());
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="l-challenges-tab">
+    <ChallengeTabHeader />
+    <div>{{ nextAtDisplay }}</div>
+    <ChallengeGrid
+      v-slot="{ challenge }"
+      :challenges="challenges"
+      :is-challenge-visible="isChallengeVisible"
+    >
+      <UltimateChallengeBox :challenge="challenge" />
+    </ChallengeGrid>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
