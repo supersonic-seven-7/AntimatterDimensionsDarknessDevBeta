813b68fbd669a70e30bf27ac717f0d7bd9f64972
Supersonic Seven <b.rockholz@icloud.com>
2025-09-03 16:33:44 -0400

    Update game.js
    Fixing EC5 reward. Please note that late-game PCs may be altered thanks to this change.

--- src/game.js
+++ src/game.js
@@ -137,7 +137,7 @@ function totalEPMult() {
 
 export function gainedEternityPoints() {
   let ep = DC.D5.pow(player.records.thisEternity.maxIP.plus(
-    gainedInfinityPoints()).log10() / (3083 - PelleRifts.recursion.effectValue.toNumber()) - 0.7).times(totalEPMult());
+    gainedInfinityPoints()).log10() / (EternityChallenge(5).reward.effectOrDefault(3083) - PelleRifts.recursion.effectValue.toNumber()) - 0.7).times(totalEPMult());
 
   if (Teresa.isRunning) {
     ep = ep.pow(0.55);
