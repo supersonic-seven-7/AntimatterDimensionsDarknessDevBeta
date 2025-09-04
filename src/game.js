870ff5a4f0b889dee19455834d066a1fea0ab507
Supersonic Seven <b.rockholz@icloud.com>
2025-09-04 12:40:05 -0400

    Update game.js
    It's funny how we can workaround these things. Hopefully by now we have a universal trigger. Of course, console.dev could trigger in cases, but if so we have another option.

--- src/game.js
+++ src/game.js
@@ -523,10 +523,14 @@ export function gameLoop(passDiff, options = {}) {
   player.celestials.ra.peakGamespeed = Math.max(player.celestials.ra.peakGamespeed, getGameSpeedupFactor());
   Enslaved.isReleaseTick = false;
 
+  if (EternityChallenge(10).completions > 0 || PlayerProgress.realityUnlocked()) {
+    player.anticheat = true;
+  }
+
   // These need to all be done consecutively in order to minimize the chance of a reset occurring between real time
   // updating and game time updating. This is only particularly noticeable when game speed is 1 and the player
   // expects to see identical numbers. We also don't increment the timers if the game has been beaten (Achievement 188)
-  if (!Achievement(188).isUnlocked && !EternityChallenge(10).completions > 0 && !PlayerProgress.realityUnlocked()) {
+  if (!Achievement(188).isUnlocked && !player.anticheat) {
     player.records.realTimeDoomed += realDiff;
     player.records.realTimePlayed += realDiff;
     player.records.totalTimePlayed += diff;
