a8364a4d135b2135f31c7c16ba46b6cbb38e59be
Supersonic Seven <b.rockholz@icloud.com>
2025-10-14 18:26:38 -0400

    Update eternity-challenges.js
    This should finalize

--- src/core/secret-formula/challenges/eternity-challenges.js
+++ src/core/secret-formula/challenges/eternity-challenges.js
@@ -201,7 +201,7 @@ export const eternityChallenges = [
     failedRestriction: "(Too slow for more)",
     reward: {
       description: "The Infinity Dimension cap threshold is raised to a power",
-      effect: completions => 1 + ((completions * (completions + 1) / 2) / 60),
+      effect: completions => 1 + (completions / 40),
       formatEffect: value => formatPow(value, 3, 3)
     }
   }
