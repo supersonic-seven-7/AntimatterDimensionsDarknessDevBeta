7fa22da70a7beb443cab25e30a08b0a9c1fa2527
Supersonic Seven <b.rockholz@icloud.com>
2025-10-15 10:56:57 -0400

    Update dilation-time-studies.js
    Acceptance

--- src/core/secret-formula/eternity/time-studies/dilation-time-studies.js
+++ src/core/secret-formula/eternity/time-studies/dilation-time-studies.js
@@ -2,7 +2,7 @@ export const dilationTimeStudies = [
   {
     id: 1,
     description: "Unlock Time Dilation",
-    cost: 999999,
+    cost: 123456,
     requirement: () => {
       const ttRequirement = Currency.timeTheorems.max.gte(TimeStudy.dilation.totalTimeTheoremRequirement);
       if (Ra.unlocks.autoUnlockDilation.canBeApplied &&
