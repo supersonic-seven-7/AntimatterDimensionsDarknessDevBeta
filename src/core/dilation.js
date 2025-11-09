a22a51e380964f4b5571efd27ae91957aba0b5d9
Supersonic Seven <b.rockholz@icloud.com>
2025-11-09 00:24:00 -0500

    Update dilation.js
    Allowing for Tachyon Gain Again Finally

--- src/core/dilation.js
+++ src/core/dilation.js
@@ -156,8 +156,7 @@ export function getDilationGainPerSecond() {
 export function tachyonGainMultiplier() {
   if (Pelle.isDisabled("tpMults")) return new Decimal(1);
   const pow = Enslaved.isRunning ? Enslaved.tachyonNerf : 1;
-  //return DC.D1.timesEffectsOf(...
-  return DC.D0.timesEffectsOf(
+  return DC.D1.timesEffectsOf(
     DilationUpgrade.tachyonGain,
     GlyphSacrifice.dilation,
     Achievement(132),
