b221961b4d758b28f56b9d823272af65659d481d
Supersonic Seven <b.rockholz@icloud.com>
2025-11-21 17:57:57 -0500

    Update GameSpeedDisplay.vue
    Wow

--- src/components/GameSpeedDisplay.vue
+++ src/components/GameSpeedDisplay.vue
@@ -40,7 +40,7 @@ export default {
       this.pulsedSpeed = getGameSpeedupForDisplay();
       this.hasSeenAlteredSpeed = PlayerProgress.seenAlteredSpeed();
       this.isStopped = Enslaved.isStoringRealTime;
-      this.isEC12 = EternityChallenge(12).isRunning;
+      this.isEC12 = EternityChallenge(12).isRunning || UltimateChallenge(3).isRunning;
       this.isPulsing = (this.baseSpeed !== this.pulsedSpeed) && Enslaved.canRelease(true);
     },
     formatNumber(num) {
