636846a9f2048b6bc59c898d0c1dc0c2b5c9c280
Supersonic Seven <b.rockholz@icloud.com>
2025-11-22 21:33:08 -0500

    Update ultimate-challenges.js
    Truly the Ultimate Challenge

--- src/core/ultimate-challenges.js
+++ src/core/ultimate-challenges.js
@@ -90,7 +90,7 @@ class UltimateChallengeState extends GameMechanicState {
   get goal() {
     if (this.id === 1) return Decimal.NUMBER_MAX_VALUE;
     if (this.id === 2) return DC.E1_6E6;
-    if (this.id === 3) return Decimal.NUMBER_MAX_VALUE;
+    if (this.id === 3) return DC.E1E15;
   }
 
   get reward() {
